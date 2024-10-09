e3fd332b50d80e3fe94cd7554058e919edc57813
banwan1212 <109507355+Banwan1212@users.noreply.github.com>
2024-10-09 11:12:12 +0800

    Update 2.sh

--- config/2.sh
+++ config/2.sh
@@ -5,7 +5,7 @@
 # 修改 子网掩码
 # sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
 
-# 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
+# 修改主机名字，把 OpenWrt-DDD 修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/OpenWrt/OpenWrt-DDD/g' package/base-files/files/bin/config_generate
 # sed -i 's/Authorization Required/wanuse.com/g' feeds/luci/modules/luci-base/ucode/template/sysauth.ut
 sed -i 's/Authorization Required/openwrt-23.05.2 wanuse.com/g' feeds/luci/themes/luci-theme-bootstrap/htdocs/luci-static/resources/view/bootstrap/sysauth.js
