d171727e8cb2c5643c5bd2028cc4569a26f69b49
banwan1212 <109507355+Banwan1212@users.noreply.github.com>
2024-10-09 11:05:10 +0800

    Update 1.sh

--- config/1.sh
+++ config/1.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-# =================================================================
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-# =================================================================
-
-# Modify default IP
 # sed -i 's/192.168.1.1/192.168.1.82/g' package/base-files/files/bin/config_generate
 # 静态 to DHCP
 sed -i 's/-static/-dhcp/g' package/base-files/files/lib/functions/uci-defaults.sh
