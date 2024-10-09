93512bb8b117558ebef631ceded9db14294cb7e0
banwan1212 <109507355+Banwan1212@users.noreply.github.com>
2024-10-09 11:31:25 +0800

    Update 2.sh

--- config/2.sh
+++ config/2.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# 修改openwrt登陆地址,把下面的 10.0.0.1 修改成你想要的就可以了
-# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-# 修改 子网掩码
-# sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
-
 # 修改主机名字，把 OpenWrt-DDD 修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/OpenWrt/OpenWrt-DDD/g' package/base-files/files/bin/config_generate
 # sed -i 's/Authorization Required/wanuse.com/g' feeds/luci/modules/luci-base/ucode/template/sysauth.ut
