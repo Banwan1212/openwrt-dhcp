805d37cab0b2108c36956b1cf11085460b3f3b79
banwan1212 <109507355+Banwan1212@users.noreply.github.com>
2024-10-10 11:01:49 +0800

    Update 4.sh

--- config/4.sh
+++ config/4.sh
@@ -35,7 +35,15 @@ popd
 # openclash
 mkdir luci-app-openclash
 cp -rf ../../kiddin9/luci-app-openclash/* luci-app-openclash
-
+#加入OpenClash核心
+chmod -R a+x $GITHUB_WORKSPACE/config/preset-clash-core.sh
+if [ "$1" = "rk33xx" ]; then
+    $GITHUB_WORKSPACE/scripts/preset-clash-core.sh arm64
+elif [ "$1" = "rk35xx" ]; then
+    $GITHUB_WORKSPACE/scripts/preset-clash-core.sh arm64
+elif [ "$1" = "x86" ]; then
+    $GITHUB_WORKSPACE/scripts/preset-clash-core.sh amd64
+fi
 #Diskman
 mkdir luci-app-diskman
 cp -rf ../../kiddin9/luci-app-diskman/* luci-app-diskman
