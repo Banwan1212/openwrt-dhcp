5c658320b4e741b908974cc08d50e718f57c2213
banwan1212 <109507355+Banwan1212@users.noreply.github.com>
2024-11-03 10:02:49 +0800

    Delete config directory

--- config/1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# sed -i 's/192.168.1.1/192.168.1.82/g' package/base-files/files/bin/config_generate
-# 静态 to DHCP
-sed -i 's/-static/-dhcp/g' package/base-files/files/lib/functions/uci-defaults.sh
-sed -i 's/none/dhcp/g' package/base-files/files/bin/config_generate
